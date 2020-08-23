4a5d5b658795ab9896ab40545502f0381ed2ac23
dword <sebastianlandw@gmail.com>
2020-08-23 10:50:26 +0000

    fix: build files

--- src/index.js
+++ src/index.js
@@ -1,45 +1,47 @@
+import modularscale from 'modularscale'
 import postcssCustomUnit from 'postcss-custom-unit'
 import postcssFunctions from 'postcss-functions'
-import modularscale from 'modularscale'
 
-export default ({ verticalRhythm = 12, fontSize = 16, capHeight = .68, modularScale } = {}) => {
-
+export default config => {
+  config = { capHeight: 0.68, fontSize: 16, verticalRhythm: 12, ...config }
   const shift = (fontSize, lineHeight) =>
-    `${(parseFloat(lineHeight) - parseFloat(fontSize) * capHeight) / 2}rem`
-
+    `${
+      (parseFloat(lineHeight) - parseFloat(fontSize) * config.capHeight) / 2
+    }rem`
   return {
     postcssPlugins: [
       postcssCustomUnit({
         units: [
           {
+            convert: value => `${modularscale(value, config.modularScale)}rem`,
             from: 'ms',
-            convert: value => `${modularscale(value, modularScale)}rem`,
           },
           {
+            convert: value =>
+              `${(value * config.verticalRhythm) / config.fontSize}rem`,
             from: 'vr',
-            convert: value => `${value * verticalRhythm / fontSize}rem`,
           },
         ],
       }),
       postcssFunctions({
         functions: {
-          shift,
           negativeShift: (fontSize, lineHeight) =>
             `-${shift(fontSize, lineHeight)}`,
+          shift,
         },
       }),
     ],
     rules: [
       {
-        type: 'helper',
-        name: 'R',
         matcher: 'R',
+        name: 'R',
         styles: {
           'font-size': '$0',
           'line-height': '$1',
-          'padding-top': 'shift($0, $1)',
           'margin-bottom': 'negativeShift($0, $1)',
+          'padding-top': 'shift($0, $1)',
         },
+        type: 'helper',
       },
     ],
   }
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,76 +1,104 @@
-import withLocalTmpDir from 'with-local-tmp-dir'
 import { endent, property } from '@dword-design/functions'
-import { outputFile } from 'fs-extra'
-import { Nuxt, Builder } from 'nuxt'
 import atomizerModule from '@dword-design/nuxt-atomizer'
 import axios from 'axios'
-import verticalRhythmPlugin from '@dword-design/atomizer-plugin-vertical-rhythm'
+import { outputFile } from 'fs-extra'
+import { Builder, Nuxt } from 'nuxt'
+import withLocalTmpDir from 'with-local-tmp-dir'
+
+import self from '.'
 
 export default {
-  'modular scale': () => withLocalTmpDir(async () => {
-    await outputFile('pages/index.js', endent`
+  'modular scale': () =>
+    withLocalTmpDir(async () => {
+      await outputFile(
+        'pages/index.js',
+        endent`
       export default {
         render: () => <div class="P(2vr) Fz(1ms)">Hello world</div>,
       }
-    `)
-    const nuxt = new Nuxt({
-      modules: [atomizerModule],
-      atomizer: {
-        plugins: [verticalRhythmPlugin({ modularScale: 'minor second' })],
-      },
-      dev: false,
-    })
-    await new Builder(nuxt).build()
-    try {
-      await nuxt.server.listen()
-      expect(axios.get('http://localhost:3000/acss.css') |> await |> property('data'))
-        .toEqual('.Fz\\(1ms\\){font-size:1.0666666666666667rem}.P\\(2vr\\){padding:1.5rem}')
-    } finally {
-      nuxt.close()
-    }
-  }),
-  rhythm: () => withLocalTmpDir(async () => {
-    await outputFile('pages/index.js', endent`
+    `
+      )
+      const nuxt = new Nuxt({
+        atomizer: {
+          plugins: [self({ modularScale: 'minor second' })],
+        },
+        dev: false,
+        modules: [atomizerModule],
+      })
+      await new Builder(nuxt).build()
+      try {
+        await nuxt.server.listen()
+        expect(
+          axios.get('http://localhost:3000/acss.css')
+            |> await
+            |> property('data')
+        ).toEqual(
+          '.Fz\\(1ms\\){font-size:1.0666666666666667rem}.P\\(2vr\\){padding:1.5rem}'
+        )
+      } finally {
+        nuxt.close()
+      }
+    }),
+  rhythm: () =>
+    withLocalTmpDir(async () => {
+      await outputFile(
+        'pages/index.js',
+        endent`
       export default {
         render: () => <div class="R(0ms,2vr)">Hello world</div>,
       }
-    `)
-    const nuxt = new Nuxt({
-      modules: [atomizerModule],
-      atomizer: {
-        plugins: [verticalRhythmPlugin()],
-      },
-      dev: false,
-    })
-    await new Builder(nuxt).build()
-    try {
-      await nuxt.server.listen()
-      expect(axios.get('http://localhost:3000/acss.css') |> await |> property('data'))
-        .toEqual('.R\\(0ms\\,2vr\\){font-size:1rem;line-height:1.5rem;padding-top:.41rem;margin-bottom:-.41rem}')
-    } finally {
-      nuxt.close()
-    }
-  }),
-  units: () => withLocalTmpDir(async () => {
-    await outputFile('pages/index.js', endent`
+    `
+      )
+      const nuxt = new Nuxt({
+        atomizer: {
+          plugins: [self()],
+        },
+        dev: false,
+        modules: [atomizerModule],
+      })
+      await new Builder(nuxt).build()
+      try {
+        await nuxt.server.listen()
+        expect(
+          axios.get('http://localhost:3000/acss.css')
+            |> await
+            |> property('data')
+        ).toEqual(
+          '.R\\(0ms\\,2vr\\){font-size:1rem;line-height:1.5rem;margin-bottom:-.41rem;padding-top:.41rem}'
+        )
+      } finally {
+        nuxt.close()
+      }
+    }),
+  units: () =>
+    withLocalTmpDir(async () => {
+      await outputFile(
+        'pages/index.js',
+        endent`
       export default {
         render: () => <div class="P(2vr) Fz(1ms)">Hello world</div>,
       }
-    `)
-    const nuxt = new Nuxt({
-      modules: [atomizerModule],
-      atomizer: {
-        plugins: [verticalRhythmPlugin()],
-      },
-      dev: false,
-    })
-    await new Builder(nuxt).build()
-    try {
-      await nuxt.server.listen()
-      expect(axios.get('http://localhost:3000/acss.css') |> await |> property('data'))
-        .toEqual('.Fz\\(1ms\\){font-size:1.61803398875rem}.P\\(2vr\\){padding:1.5rem}')
-    } finally {
-      nuxt.close()
-    }
-  }),
+    `
+      )
+      const nuxt = new Nuxt({
+        atomizer: {
+          plugins: [self()],
+        },
+        dev: false,
+        modules: [atomizerModule],
+      })
+      await new Builder(nuxt).build()
+      try {
+        await nuxt.server.listen()
+        expect(
+          axios.get('http://localhost:3000/acss.css')
+            |> await
+            |> property('data')
+        ).toEqual(
+          '.Fz\\(1ms\\){font-size:1.61803398875rem}.P\\(2vr\\){padding:1.5rem}'
+        )
+      } finally {
+        nuxt.close()
+      }
+    }),
 }
